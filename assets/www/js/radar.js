573bd4f5ceadb021046a159f2b315e061e8b8053
nubela <nubela@gmail.com>
2011-09-17 04:28:50 +0800

    WIP, working hover, also page redirects, and icons

--- assets/www/js/radar.js
+++ assets/www/js/radar.js
@@ -2,6 +2,30 @@ var feedUrl = "http://192.168.1.120:5000";
 
 $(document).ready(function() {
 	
+	$(".go_categories")[0].addEventListener('touchstart',function(e) {
+		 $(this).attr("src","images/categories_s.png");
+	});
+	
+	$(".go_camera")[0].addEventListener('touchstart',function(e) {
+		 $(this).attr("src","images/camera_s.png");
+	});
+	
+	$(".go_user")[0].addEventListener('touchstart',function(e) {
+		 $(this).attr("src","images/user_s.png");
+	});
+	
+	$(".go_categories")[0].addEventListener('touchend',function(e) {
+		 window.location="categories.html";
+	});
+	
+	$(".go_user")[0].addEventListener('touchend',function(e) {
+		 window.location="user.html";
+	});
+	
+	$(".go_camera")[0].addEventListener('touchend',function(e) {
+		 window.location="camera1.html";
+	});
+	
 });
 
 function generalFailure() {
@@ -27,9 +51,15 @@ function initFrontpage(latitude, longitude) {
 			var ads = data.ads;
 			for (i = 0; i < ads.length; i++) {
 				var ad = ads[i];
+				
+				var title = ad.title;
+				if (title.length > 20) {
+					title = title.substring(0,20) + "..";
+				}
+				
 				var container = $(".specialcontainer").clone().removeClass("specialcontainer").removeClass("hidden").addClass("itemcontainer");
-				$(container).find(".1").html(ad.title);
-				$(container).find(".price").html(ad.price);
+				$(container).find(".1").html(title);
+				$(container).find(".price").html("$" + ad.price);
 				$(container).find(".category").html(ad.category.name);
 				$(container).find(".thumbimg").attr("src",feedUrl + "/static/uploads/" + ad.image);
 				$(".specialcontainer").after(container);
